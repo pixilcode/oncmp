a07b7037d39734300f7de328d13b5a06afabfe26
Brendon Bown <brendon@careweather.com>
2026-03-02 15:17:51 -0700

    fix: Don't save regression output to a file

--- run_old_regression.sh
+++ run_old_regression.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
 # this script should be run from the root of
 # the veery_old repository
 
 source .venv/bin/activate
 cd model
-oneil regression-test radar.on > "${original_dir}/old.out" 2>&1
+oneil regression-test radar.on
